7db2996ed6b6f021738022e3828179bac283205f
💤 <ongongfrfr@outlook.com.au>
2026-04-07 17:32:12 +1000

    bot can text

--- main.go
+++ main.go
@@ -3,23 +3,57 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	token := os.Getenv("DISCORD_BOT_TOKEN")
-
 	session, err := discordgo.New("Bot " + token)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Events bot can receive (should be the minimum possible for least overhead)
+	session.Identify.Intents = discordgo.IntentsAllWithoutPrivileged | discordgo.IntentsMessageContent
+
+	// session.AddHandler()
+
+	if err = session.Open(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer log.Println("Bot offline.")
+	defer session.Close()
+
+	log.Println("Bot online! Press CTRL-C to exit.")
+
+	// Test example
+	//_, err = session.ChannelMessageSend("1490700697681924147", "aaaaa")
+
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+
+	// Blocks until CTRL-C or other signal is received
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
+
+// Test example: Sends "sup" whenever someone else says "sup"
+func sendSup() {
 
 }
 
