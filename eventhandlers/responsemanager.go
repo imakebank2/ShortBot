8bcee20bfd3e95bbd5a46974ae1bb60e7e79454d
ong ong fr fr <ongongfrfr@outlook.com.au>
2026-04-08 08:00:06 +1000

    Correct function name typo in response manager
    Fix typo in function name from 'ReponseManager' to 'ResponseManager'.

--- eventhandlers/responsemanager.go
+++ eventhandlers/responsemanager.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Manages the bot's automatic (non-command) responses
+// Runs every time someone sends a message
 func ReponseManager(s *discordgo.Session, m *discordgo.MessageCreate) {
 	responses.SendSup(s, m)
 }
