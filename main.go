b166f857d94f62b0be236a7db2e79e5a62e64c45
💤 <ongongfrfr@outlook.com.au>
2026-04-07 15:09:09 +1000

    stuff

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	discordgo.New("")
-	fmt.Println("Hello")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("DISCORD_BOT_TOKEN")
+
+	session, err := discordgo.New("Bot " + token)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 // "Rewriting" it from Python to Go since it is way better for free/cheap hosting since it is much more performant + uses less storage and ram
