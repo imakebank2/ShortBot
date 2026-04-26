3f7e31fe0abcd7ef023a8a6fa868027558f35943
💤 <ongongfrfr@outlook.com.au>
2026-04-26 20:56:33 +1000

    tungtungtungsahur

---
+++ commands/actions/tung.go
@@ -0,0 +1,14 @@
+package actions
+
+import (
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func Tung(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	interactionTextResponse(strings.Repeat("tung ", 100), s, i)
+	s.ChannelMessageSend(i.ChannelID,
+		"https://tenor.com/view/tung-tungtung-tungtungtung-sahur-tungtungtungsahur-tungtungsahur-gif-6699270143817937548",
+	)
+}
